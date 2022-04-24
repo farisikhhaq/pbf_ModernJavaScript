0c1acd2cfdb79468b6424c0384f4345991199857
farisikhhaq <farisikhlasulhaq@yahoo.co.id>
2022-04-24 15:56:08 +0700

    some updates

--- Tugas 8/tugas8/src/Header.jsx
+++ Tugas 8/tugas8/src/Header.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import routes from "./";
+import React from 'react';
+import routes from "./routes";
 import { Link } from "react-router-dom";
 
 const Header = () => (
--- Tugas 8/tugas8/src/index.js
+++ Tugas 8/tugas8/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import routes from "./router";
+import routes from "./routes";
 import Header from "./Header";
 import "./style.css"
 import * as firebase from "firebase";
